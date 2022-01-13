327b9bdf7efaf16cf6a18c4412b95ab15eb6fc8a
Gamer2097 <63710393+Gamer2097@users.noreply.github.com>
2022-01-13 23:48:05 +0100

    Add files via upload

---
+++ ZoomIn/Class1.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using HarmonyLib;
+using BepInEx.Configuration;
+
+namespace ZoomIn
+{
+    [BepInPlugin("Zoomin", "Zoomin", "1.0.1")]
+    public class Zoomin : BaseUnityPlugin
+    {
+        public static ConfigEntry<int> Fieldofview { get; private set; }
+        private static BepInEx.Logging.ManualLogSource logger { get; set; }
+        private void Awake()
+        {
+            
+            Fieldofview = Config.Bind("Field of View value",
+                                      "FoV",
+                                      80,
+                                      "Sets the FoV when you play a track");
+            Harmony.CreateAndPatchAll(typeof(cameraPatches));
+        }
+        
+        public class cameraPatches
+        {
+            [HarmonyPatch(typeof(Track), nameof(Track.PlayTrack)), HarmonyPostfix]
+            private static void PlayTrack_Postfix()
+            {
+                GameplayVariables.Instance.camFieldOfView = Fieldofview.Value;
+            }
+        }
+    }
+}
